4d25b798bddbcdaf72dba2b5cee5f054deccfab7
Nikita <74291699+Nikittua@users.noreply.github.com>
2026-06-21 02:42:23 +0300

    Update boot_from_volume_live_migrate_and_extend.py
    Add split by tasks to measure time

--- boot_from_volume_live_migrate_and_extend.py
+++ boot_from_volume_live_migrate_and_extend.py
@@ -1,9 +1,12 @@
 import time
 
-from novaclient import exceptions as nova_exc
 from cinderclient import exceptions as cinder_exc
+from novaclient import exceptions as nova_exc
+
 from rally.task import scenario
 from rally.task import validation
+from rally.task import atomic
+
 from rally_openstack.task.scenarios.nova import utils as nova_utils
 
 
@@ -57,6 +60,32 @@ class BootFromVolumeLiveMigrateAndExtend(nova_utils.NovaScenario):
             (volume_id, status)
         )
 
+    def _wait_for_volume_status_any(self, cinder, volume_id, statuses,
+                                    timeout=600, check_interval=2):
+        wanted = [s.lower() for s in statuses]
+        start = time.time()
+        while time.time() - start < timeout:
+            try:
+                vol = cinder.volumes.get(volume_id)
+            except cinder_exc.NotFound:
+                raise RuntimeError(
+                    "Volume %s not found while waiting for statuses %s" %
+                    (volume_id, statuses)
+                )
+
+            cur = (vol.status or "").lower()
+            if cur in wanted:
+                return vol
+            if cur == "error":
+                raise RuntimeError("Volume %s entered ERROR state" % volume_id)
+
+            time.sleep(check_interval)
+
+        raise RuntimeError(
+            "Timeout waiting for volume %s to become one of %s" %
+            (volume_id, statuses)
+        )
+
     def _wait_for_server_status(self, server_id, status,
                                 timeout=600, check_interval=2):
         nova = self.clients("nova")
@@ -93,8 +122,9 @@ class BootFromVolumeLiveMigrateAndExtend(nova_utils.NovaScenario):
             except nova_exc.NotFound:
                 return
             time.sleep(check_interval)
-        raise RuntimeError("Timeout waiting for server %s to be deleted" %
-                           server_id)
+        raise RuntimeError(
+            "Timeout waiting for server %s to be deleted" % server_id
+        )
 
     def _wait_for_volume_delete(self, cinder, volume_id,
                                 timeout=600, check_interval=2):
@@ -105,10 +135,87 @@ class BootFromVolumeLiveMigrateAndExtend(nova_utils.NovaScenario):
             except cinder_exc.NotFound:
                 return
             time.sleep(check_interval)
-        raise RuntimeError("Timeout waiting for volume %s to be deleted" %
-                           volume_id)
+        raise RuntimeError(
+            "Timeout waiting for volume %s to be deleted" % volume_id
+        )
 
-    def _safe_delete_server(self, server_id):
+    @atomic.action_timer("custom.create_boot_volume")
+    def _atomic_create_boot_volume(self, cinder, glance_image,
+                                   volume_size, volume_type):
+        create_args = {
+            "size": volume_size,
+            "imageRef": glance_image.id,
+            "name": self.generate_random_name()
+        }
+        if volume_type:
+            create_args["volume_type"] = volume_type
+
+        volume = cinder.volumes.create(**create_args)
+        self._wait_for_volume_status(cinder, volume.id, "available")
+        return volume
+
+    @atomic.action_timer("custom.boot_server_from_volume")
+    def _atomic_boot_server_from_volume(self, nova, nova_flavor,
+                                        volume_id, auto_assign_nic):
+        bdm = [{
+            "uuid": volume_id,
+            "boot_index": 0,
+            "source_type": "volume",
+            "destination_type": "volume",
+            "delete_on_termination": False
+        }]
+
+        # Можно потом доработать выбор сетей, пока как было у тебя
+        nics = None if auto_assign_nic else None
+
+        server = nova.servers.create(
+            name=self.generate_random_name(),
+            image=None,
+            flavor=nova_flavor,
+            nics=nics,
+            block_device_mapping_v2=bdm
+        )
+        self._wait_for_server_status(server.id, "ACTIVE")
+        return server
+
+    @atomic.action_timer("custom.live_migrate_server")
+    def _atomic_live_migrate(self, nova, server_id,
+                             block_migration, disk_over_commit, cinder,
+                             volume_id):
+        nova.servers.live_migrate(
+            server=server_id,
+            host=None,
+            block_migration=block_migration,
+            disk_over_commit=disk_over_commit
+        )
+        self._wait_for_server_status(server_id, "ACTIVE")
+        self._wait_for_volume_status(cinder, volume_id, "in-use")
+
+    @atomic.action_timer("custom.reset_volume_state_available")
+    def _atomic_reset_volume_state(self, cinder, volume_id):
+        try:
+            cinder.volumes.reset_state(volume_id, state="available")
+        except Exception as exc:
+            raise RuntimeError(
+                "Failed to reset volume %s state to available: %s" %
+                (volume_id, exc)
+            )
+        self._wait_for_volume_status(cinder, volume_id, "available")
+
+    @atomic.action_timer("custom.extend_volume")
+    def _atomic_extend_volume(self, cinder, volume_id, new_volume_size):
+        cinder.volumes.extend(volume_id, new_volume_size)
+        self._wait_for_volume_status_any(
+            cinder, volume_id, ["available", "in-use"], timeout=600
+        )
+        final_vol = cinder.volumes.get(volume_id)
+        if int(final_vol.size) != int(new_volume_size):
+            raise RuntimeError(
+                "Volume %s size is %s, expected %s" %
+                (volume_id, final_vol.size, new_volume_size)
+            )
+
+    def _delete_server(self, server_id):
         nova = self.clients("nova")
         try:
             nova.servers.delete(server_id)
@@ -116,13 +223,12 @@ class BootFromVolumeLiveMigrateAndExtend(nova_utils.NovaScenario):
             return
         except Exception:
             return
-
         try:
             self._wait_for_server_delete(server_id, timeout=300)
         except Exception:
             pass
 
-    def _safe_delete_volume(self, cinder, volume_id):
+    def _delete_volume(self, cinder, volume_id):
         try:
             start = time.time()
             while time.time() - start < 300:
@@ -168,90 +274,35 @@ class BootFromVolumeLiveMigrateAndExtend(nova_utils.NovaScenario):
         volume_id = None
 
         try:
-            # 1. Создаём bootable volume из образа
-            create_args = {
-                "size": volume_size,
-                "imageRef": glance_image.id,
-                "name": self.generate_random_name()
-            }
-            if volume_type:
-                create_args["volume_type"] = volume_type
-
-            volume = cinder.volumes.create(**create_args)
+            # 1. Создать bootable том
+            volume = self._atomic_create_boot_volume(
+                cinder, glance_image, volume_size, volume_type
+            )
             volume_id = volume.id
-            self._wait_for_volume_status(cinder, volume_id, "available")
 
-            # 2. Boot from volume
-            bdm = [{
-                "uuid": volume_id,
-                "boot_index": 0,
-                "source_type": "volume",
-                "destination_type": "volume",
-                "delete_on_termination": False
-            }]
-
-            nics = None if auto_assign_nic else None
-
-            server = nova.servers.create(
-                name=self.generate_random_name(),
-                image=None,
-                flavor=nova_flavor,
-                nics=nics,
-                block_device_mapping_v2=bdm
+            # 2. Стартовать ВМ с тома
+            server = self._atomic_boot_server_from_volume(
+                nova, nova_flavor, volume_id, auto_assign_nic
             )
             server_id = server.id
 
-            self._wait_for_server_status(server_id, "ACTIVE")
-            self._wait_for_volume_status(cinder, volume_id, "in-use")
-
             # 3. Live migration
-            nova.servers.live_migrate(
-                server=server_id,
-                host=None,
-                block_migration=block_migration,
-                disk_over_commit=disk_over_commit
+            self._atomic_live_migrate(
+                nova, server_id, block_migration, disk_over_commit,
+                cinder, volume_id
             )
 
-            self._wait_for_server_status(server_id, "ACTIVE")
-            self._wait_for_volume_status(cinder, volume_id, "in-use")
+            # 4. Reset state тома
+            self._atomic_reset_volume_state(cinder, volume_id)
 
-            # 4. Reset-state root‑тома в available (админский трюк)
-            try:
-                cinder.volumes.reset_state(volume_id, state="available")
-            except Exception as exc:
-                raise RuntimeError(
-                    "Failed to reset volume %s state to available: %s" %
-                    (volume_id, exc)
-                )
-
-            self._wait_for_volume_status(cinder, volume_id, "available")
-
-            # 5. Extend до new_volume_size
+            # 5. Extend тома (если требуется)
             if new_volume_size and new_volume_size > volume_size:
-                cinder.volumes.extend(volume_id, new_volume_size)
-
-                # ждём, пока том перейдёт в приемлемое состояние
-                start = time.time()
-                while time.time() - start < 600:
-                    vol = cinder.volumes.get(volume_id)
-                    status = (vol.status or "").lower()
-                    if status in ("available", "in-use"):
-                        break
-                    if status == "error":
-                        raise RuntimeError(
-                            "Volume %s entered ERROR after extend" % volume_id
-                        )
-                    time.sleep(2)
-
-                final_vol = cinder.volumes.get(volume_id)
-                if int(final_vol.size) != int(new_volume_size):
-                    raise RuntimeError(
-                        "Volume %s size is %s, expected %s"
-                        % (volume_id, final_vol.size, new_volume_size)
-                    )
+                self._atomic_extend_volume(
+                    cinder, volume_id, new_volume_size
+                )
 
         finally:
             if server_id:
-                self._safe_delete_server(server_id)
+                self._delete_server(server_id)
             if volume_id:
-                self._safe_delete_volume(cinder, volume_id)
+                self._delete_volume(cinder, volume_id)
